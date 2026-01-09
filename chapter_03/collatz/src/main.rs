aee90b6db8d55f912d9d33381eac400acea2b4b8
Thomas-Hutch <htchnstm@gmail.com>
2026-01-09 21:03:04 +0000

    Chapter 3 - The collatz problem

---
+++ chapter_03/collatz/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let mut chain_length: u32 = 0;
+    let mut chain_number: u32 = 0;
+
+    for number in 1..=1000 {
+        let count = count_steps(number);
+        if count > chain_length {
+            chain_length = count;
+            chain_number = number;
+        }
+    }
+    println!("The number {} has the longest chain: {} steps", chain_number, chain_length);
+}
+
+fn count_steps(number: u32) -> u32 {
+    let mut count = 0;
+    let mut number = number;
+    while number != 1 {
+        if number % 2 == 0 {
+            number /= 2;
+        } else {
+            number = (number * 3) + 1;            
+        }
+        count += 1;
+    }
+    count
+}
