4d9f009a87ad5fe6ee120e4c193efc7297018b77
Thomas-Hutch <htchnstm@gmail.com>
2026-01-09 21:19:39 +0000

    Chapter 4 - file extensions

---
+++ chapter_04/file_extension/src/main.rs
@@ -0,0 +1,17 @@
+fn get_extension(filename: &str) -> &str {
+    let bytes = filename.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate().rev() {
+        if item == b'.' {
+            return &filename[i..];
+        }
+    }
+    &filename
+}
+
+fn main() {
+    let file = "image.png.txt";
+    let ext = get_extension(file);
+    
+    println!("Extension: {}", ext); 
+}
